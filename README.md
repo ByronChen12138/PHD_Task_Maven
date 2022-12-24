63f50a6f231467ec2ada050ef5d35a03ebd09af7
ByronChen12138 <yifei.chen@mail.mcgill.ca>
2022-12-24 02:46:39 -0500

    fin

--- README.md
+++ README.md
@@ -1,5 +1,61 @@
-# PHD_Task
-PHD_Task
+# PHD Task
+This is the PHD Task  from Prof. Lili Wei and is done by Byron Chen.
+In this task, package Soot is used with some simple methods.
+
+---
+## Notes
+
+### Classes
+**[BodyTransformer](https://soot-build.cs.uni-paderborn.de/public/origin/develop/soot/soot-develop/jdoc/soot/BodyTransformer.html)** - any customized analysis should extend this class,
+This method is invoked once for every method in the analysis subject. By using:
+``` java
+String jarFilePath = <Path of the Subject>;
+String[] sootArgs = {
+    <cmd lines for OS>
+};
+
+// InterIntegrate customized analysis code to Soot.
+Pack jtp = PackManager.v().getPack("jtp");
+jtp.add(new Transform("jtp.MyTrans", 
+        new <Function that extend BodyTransformer for analysis>()));
+
+//Invoke the main method of Soot to perform the analysis
+Main.main(sootArgs);
+```
+
+**[internalTransform](https://soot-build.cs.uni-paderborn.de/public/origin/develop/soot/soot-develop/jdoc/soot/BodyTransformer.html)** - This 
+method is called to perform the transformation itself. It is declared abstract; subclasses must implement this method by making it the entry point to their actual Body transformation.
+
+```java
+protected abstract void internalTransform(Body b, 
+                                          String phaseName, 
+                                          Map<String, String> options)
+```
+
+**[Body](https://soot-build.cs.uni-paderborn.de/public/origin/develop/soot/soot-develop/jdoc/soot/JastAddJ/Body.html)** - the
+body of the code for applying the transformation
+
+**[SootMethod](https://soot-build.cs.uni-paderborn.de/public/origin/develop/soot/soot-develop/jdoc/soot/SootMethod.html)** - Soot representation of a Java method. 
+Can be declared to belong to a SootClass. Does not contain the actual code, which belongs to a Body. 
+The `getActiveBody()` method points to the currently-active body.
+
+**[Chain](https://soot-build.cs.uni-paderborn.de/public/origin/develop/soot/soot-develop/jdoc/soot/util/Chain.html)** - 
+Augmented data type guaranteeing O(1) insertion and removal from a set of ordered, unique elements.
+
+**[Stmt](https://soot-build.cs.uni-paderborn.de/public/origin/develop/soot/soot-develop/jdoc/soot/JastAddJ/Stmt.html)** -
+Statements
+
+**[AssignStmt](https://soot-build.cs.uni-paderborn.de/public/origin/develop/soot/soot-develop/jdoc/soot/jimple/AssignStmt.html)** -
+Assignment Statements
+
+**[InvokeStmt](https://soot-build.cs.uni-paderborn.de/public/origin/develop/soot/soot-develop/jdoc/soot/jimple/InvokeStmt.html)** -
+Method Invoke Statements
+
+
+
+---
+## ATTENTION
+Different java files in the same jar pack are run in multi-threads which might lead some unexpected consequence.
 
 ---
 ## Commands
--- src/main/java/MainDriver.java
+++ src/main/java/MainDriver.java
@@ -1,14 +1,22 @@
 import soot.*;
-import soot.jimple.IfStmt;
+import soot.jimple.AssignStmt;
+import soot.jimple.InvokeStmt;
 import soot.jimple.Stmt;
 import soot.util.Chain;
 
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
+
+class MethodCount {
+    public static int method_cnt;
+}
 
 public class MainDriver {
     // The main method to execute the tool
     public static void main(String[] args) {
+
+
         // Specify the path to the analysis subject jar file. Remember to change it to the path where you save subject.jar
         String jarFilePath = "./packages/subject.jar";
 
@@ -23,37 +31,49 @@ public class MainDriver {
 
         // Integrate your customized analysis code to Soot. The customized code is implemented in class IfScanner.
         Pack jtp = PackManager.v().getPack("jtp");
-        jtp.add(new Transform("jtp.MyTrans", new IfScanner()));
+        jtp.add(new Transform("jtp.MyTrans", new TaskScanner()));
 
-        //Invoke the main method of Soot to perform the analysis
+        MethodCount.method_cnt = 0;
+
+        // Invoke the main method of Soot to perform the analysis
         Main.main(sootArgs);
+
+        // Output
+        System.out.println("The total number of analyzed methods: " + MethodCount.method_cnt);
     }
 }
 
-// The class containing our customized analysis. It should extend class BodyTransformer
-class IfScanner extends BodyTransformer {
-    // This method is invoked once for every method in the analysis subject.
-    // You may refer to Soot's JavaDoc to check the meaning of the parameters of this method.
+class TaskScanner extends BodyTransformer {
     @Override
     protected void internalTransform(Body body, String s, Map<String, String> map) {
-        // This method is to calculate the number of if statements in each method in the analysis subject.
-        int if_cnt = 0;
+        int assign_cnt = 0;
+        int invoke_cnt = 0;
+
+        MethodCount.method_cnt++;
         SootMethod method = body.getMethod();
         String sig = method.getSignature(); // Get the signature of the method being analyzed
 
-        //Get an iterator to iterate over all the statements in the method being analyzed
+        // If not wanted method, return
+        if (!Objects.equals(sig, "<Sort: void heapSort(int[])>")) return;
+
+        // Get an iterator to iterate over all the statements in the method being analyzed
         Chain chain = body.getUnits();
         Iterator<Unit> unitIterator = chain.snapshotIterator();
 
         while (unitIterator.hasNext()) {
             Stmt stmt = (Stmt) unitIterator.next();
-            // Check whether stmt is an if statement. If so, add one to the counter.
-            if (stmt instanceof IfStmt) {
-                if_cnt++;
+
+            if (stmt instanceof AssignStmt) {
+                assign_cnt++;
+            }
+
+            if (stmt instanceof InvokeStmt) {
+                invoke_cnt++;
             }
         }
 
-        // Print out the signature of the method being analyzed and the number of if statements in this method.
-        System.out.println(sig + ", " + if_cnt);
+        // Output
+        System.out.println("Number of assignment statements in the target method: " + assign_cnt);
+        System.out.println("Number of statements containing method invocations in the target method: " + invoke_cnt);
     }
 }
